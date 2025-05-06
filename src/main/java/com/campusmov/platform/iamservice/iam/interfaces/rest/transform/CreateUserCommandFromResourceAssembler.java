ac996f1d2af8d6d4db9ca39647f0333ab03262f7
Eric <erich56000@gmail.com>
2025-05-06 12:01:49 -0500

    feat(registration): add transformation interface for user and create user

---
+++ src/main/java/com/campusmov/platform/iamservice/iam/interfaces/rest/transform/CreateUserCommandFromResourceAssembler.java
@@ -0,0 +1,10 @@
+package com.campusmov.platform.iamservice.iam.interfaces.rest.transform;
+
+import com.campusmov.platform.iamservice.iam.domain.model.commands.CreateUserCommand;
+import com.campusmov.platform.iamservice.iam.interfaces.rest.resources.CreateUserResource;
+
+public class CreateUserCommandFromResourceAssembler {
+    public static CreateUserCommand toCommandFromResource(CreateUserResource resource) {
+        return new CreateUserCommand(resource.email(), resource.password(), resource.roleId());
+    }
+}
---
+++ src/main/java/com/campusmov/platform/iamservice/iam/interfaces/rest/transform/UserResourceFromEntityAssembler.java
@@ -0,0 +1,10 @@
+package com.campusmov.platform.iamservice.iam.interfaces.rest.transform;
+
+import com.campusmov.platform.iamservice.iam.domain.model.aggregates.User;
+import com.campusmov.platform.iamservice.iam.interfaces.rest.resources.UserResource;
+
+public class UserResourceFromEntityAssembler {
+    public static UserResource toResourceFromEntity(User user) {
+        return new UserResource(user.getId(), user.getEmail(), user.getRoles());
+    }
+}
