d832d80f8440900008e1fd70e00bbced60f59c8e
Eric <erich56000@gmail.com>
2025-05-06 12:00:00 -0500

    feat(registration): implement user command service

---
+++ src/main/java/com/campusmov/platform/iamservice/iam/application/internal/commandservices/UserCommandServiceImpl.java
@@ -0,0 +1,36 @@
+package com.campusmov.platform.iamservice.iam.application.internal.commandservices;
+
+
+import com.campusmov.platform.iamservice.iam.domain.model.aggregates.User;
+import com.campusmov.platform.iamservice.iam.domain.model.commands.CreateUserCommand;
+import com.campusmov.platform.iamservice.iam.domain.services.UserCommandService;
+import com.campusmov.platform.iamservice.iam.infrastructure.persistence.jpa.repositories.RoleRepository;
+import com.campusmov.platform.iamservice.iam.infrastructure.persistence.jpa.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserCommandServiceImpl implements UserCommandService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public UserCommandServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Optional<User> handle(CreateUserCommand command) {
+        if (userRepository.existsByEmail(command.email())) throw new RuntimeException("User " + command.email() + " already exists");
+
+        var roles = command.roleId().stream()
+                .map(roleId -> roleRepository.findById(roleId)
+                        .orElseThrow(() -> new RuntimeException("Role with ID " + roleId + " not found")))
+                .toList();
+
+        var user = new User(command.email(), command.password(), roles);
+        userRepository.save(user);
+        return Optional.of(user);
+    }
+}
---
+++ src/main/java/com/campusmov/platform/iamservice/iam/domain/services/UserCommandService.java
@@ -0,0 +1,11 @@
+package com.campusmov.platform.iamservice.iam.domain.services;
+
+import com.campusmov.platform.iamservice.iam.domain.model.aggregates.User;
+import com.campusmov.platform.iamservice.iam.domain.model.commands.CreateUserCommand;
+
+import java.util.Optional;
+
+public interface UserCommandService {
+
+    Optional<User> handle(CreateUserCommand command);
+}
