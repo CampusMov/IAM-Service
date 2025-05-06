3bd8592627102511c100345465b18b0974e689bb
Eric <erich56000@gmail.com>
2025-05-06 01:58:47 -0500

    feat: implement JPA auditing and OpenAPI and snake_case pluralized naming strategy

--- src/main/java/com/campusmov/platform/iamservice/IamServiceApplication.java
+++ src/main/java/com/campusmov/platform/iamservice/IamServiceApplication.java
@@ -2,8 +2,10 @@ package com.campusmov.platform.iamservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class IamServiceApplication {
 
     public static void main(String[] args) {
