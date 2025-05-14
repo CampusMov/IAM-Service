7a73ca8c4d31b67d5f918eabf7472a3c445eeb01
Eric <erich56000@gmail.com>
2025-05-14 00:56:10 -0500

    refactor(login): implement institutional email validation

--- src/main/java/com/campusmov/platform/iamservice/iam/interfaces/rest/resources/CreateUserResource.java
+++ src/main/java/com/campusmov/platform/iamservice/iam/interfaces/rest/resources/CreateUserResource.java
@@ -1,14 +1,21 @@
 package com.campusmov.platform.iamservice.iam.interfaces.rest.resources;
 
-import java.util.List;
+import java.util.regex.Pattern;
 
-public record CreateUserResource(String email, List<String> roleName) {
+public record CreateUserResource(String email) {
     public CreateUserResource {
         if (email == null || email.isBlank()) {
             throw new IllegalArgumentException("Email cannot be null or empty");
         }
-        if (roleName == null || roleName.isEmpty()) {
-            throw new IllegalArgumentException("Role ID cannot be null or empty");
+
+        boolean emailPattern = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$").matcher(email).matches();
+
+        boolean universityDomainPattern = email.endsWith(".edu.pe");
+
+        if (!emailPattern || !universityDomainPattern) {
+            throw new IllegalArgumentException("Invalid email format");
         }
+
+
     }
 }
