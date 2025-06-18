ade3d8edc268fac9f0be26e2909dcbb7899eebaa
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-06-18 00:26:29 -0500

    feat: add active user command

--- src/main/java/com/campusmov/platform/iamservice/iam/domain/services/UserCommandService.java
+++ src/main/java/com/campusmov/platform/iamservice/iam/domain/services/UserCommandService.java
@@ -1,6 +1,7 @@
 package com.campusmov.platform.iamservice.iam.domain.services;
 
 import com.campusmov.platform.iamservice.iam.domain.model.aggregates.User;
+import com.campusmov.platform.iamservice.iam.domain.model.commands.ActivateUserCommand;
 import com.campusmov.platform.iamservice.iam.domain.model.commands.CreateUserCommand;
 
 import java.util.Optional;
@@ -8,4 +9,5 @@ import java.util.Optional;
 public interface UserCommandService {
 
     Optional<User> handle(CreateUserCommand command);
+    void handle(ActivateUserCommand command);
 }
